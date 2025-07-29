0dcc7ef949f887cafbfa15dc4d0c171944d0d75a
aycamentess <ayca.mentes@sportvy.com>
2025-07-29 09:55:04 +0300

    favorilere ekleme ve favori sayfası yapıldı.anasayfadaki kategori filtreleme ekranı düzenlendi.

--- src/screens/SettingPage.tsx
+++ src/screens/SettingPage.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function SettingPage() {
@@ -8,3 +8,5 @@ export default function SettingPage() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({})
